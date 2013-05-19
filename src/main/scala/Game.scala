ec1b95edb63502efec8392ff38a3c35bb0554cd8
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2013-05-19 18:28:09 +0200

    working text input circle

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -7,16 +7,10 @@ import scala.concurrent.Await
 import akka.pattern.ask
 import akka.util.Timeout
 
-import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
-import org.newdawn.slick.SlickException;
-import org.newdawn.slick.InputListener
-import org.newdawn.slick.ControllerListener
+import org.newdawn.slick._
 import org.newdawn.slick.command._
-
+import org.newdawn.slick.font.effects.{ColorEffect, Effect}
+import org.newdawn.slick.geom.{Rectangle, Transform}
 
 object Config {
   import com.typesafe.config.ConfigFactory
@@ -52,10 +46,14 @@ class InputProviderTest extends BasicGame("InputProvider Test") {
   val inputHandler = Config.system.actorOf(Props[InputActor], name = "inputHandler")
   val inputLogger = Config.system.actorOf(Props[InputLogger], name = "inputLogger")
   Config.system.eventStream.subscribe(inputLogger, classOf[ButtonEvent])
+  inputHandler ! SubscribeAll(inputLogger)
 
   import Config.system.dispatcher
   import scala.concurrent.duration._
   implicit val timeout = Timeout(1 seconds)
+
+  var font: UnicodeFont = _;
+
 
   Config.system.scheduler.schedule(0 milliseconds,
       10 milliseconds,
@@ -63,38 +61,123 @@ class InputProviderTest extends BasicGame("InputProvider Test") {
       Poll)
 
   def init(container: GameContainer) {
-    Await.result(inputHandler ? "start", timeout.duration)
+    Await.result(inputHandler ? Start, timeout.duration)
+
+    font = new UnicodeFont("res/fonts/DroidSansMonoDotted.ttf", 20, true, false);
+    font.addAsciiGlyphs();
+    font.getEffects() match {
+      case effects: java.util.List[Effect] => effects.add(new ColorEffect());
+    }
+      // Create a default white color effect
+    font.loadGlyphs();
   }
 
   var controllersToDraw = List[Int]()
 
   def render(container: GameContainer, g: Graphics) {
+    g.setColor(Color.white)
     g.drawString("Press any button to register a controller", 10, 50);
     g.drawString(message,100,150);
-
-
 
     var offset = 0
     for(c <- controllersToDraw) {
       val pad = axes(c)
 
       g.drawOval(80, 80 + offset, 100, 100)
-      g.drawOval(125 + pad.leftStickX * 50, 125 + pad.leftStickY * 50 + offset, 10, 10)
+      g.fillOval(125 + pad.leftStickX * 50, 125 + pad.leftStickY * 50 + offset, 10, 10)
 
       g.drawOval(230, 80 + offset, 100, 100)
-      g.drawOval(275 + pad.rightStickX * 50, 125 + pad.rightStickY * 50 + offset, 10, 10)
+      g.fillOval(275 + pad.rightStickX * 50, 125 + pad.rightStickY * 50 + offset, 10, 10)
 
       g.drawRect(380, 80 + offset, 20, 100)
-      g.drawOval(385, 125 + pad.leftTrigger * 50 + offset, 10, 10)
+      g.fillOval(385, 125 + pad.leftTrigger * 50 + offset, 10, 10)
 
       g.drawRect(430, 80 + offset, 20, 100)
-      g.drawOval(435, 125 + pad.rightTrigger * 50 + offset, 10, 10)
+      g.fillOval(435, 125 + pad.rightTrigger * 50 + offset, 10, 10)
 
       g.drawString(s"controller $c", 470, 120 + offset)
 
       offset += 130
     }
 
+    var hightlightedCircle = -1
+
+    for(c <- controllersToDraw) {
+      val pad = axes(c)
+      if(Math.abs(pad.leftStickX) > 0.1 || Math.abs(pad.leftStickY) > 0.1) {
+        val phi = Math.atan2(pad.leftStickX, pad.leftStickY)
+        hightlightedCircle = (12 - (phi * 8 / (2*Math.PI) + 0.5).toInt) % 8
+      }
+    }
+
+    val darkYellow = new Color(179, 142, 31)
+    val darkBlue = new Color(25, 66, 127)
+    val darkRed = new Color(165, 28, 12)
+    val darkGreen = new Color(107, 144, 12)
+    val circleHighlighted = new Color(56, 91, 112)
+    val circleNormal = new Color(33, 57, 71)
+    val darkBackgroundCircle = new Color(28, 49, 61)
+    val transparent = List(Color.transparent, Color.transparent, Color.transparent, Color.transparent)
+    val colors = List(darkBlue, darkYellow, darkRed, darkGreen)
+    val o = 40
+    val offsets = List( (-o, 0), (0, -o), (o, 0), (0, o) )
+
+    val chars = ('a' to 'z').map(_.toString) ++ List(",", ".", ":", "/", "@", "-")
+    val charGroupCircleRadius = 70
+    val charGroupCircleOffset = 185
+    val backgroundCircleRadius = charGroupCircleOffset + charGroupCircleRadius +
+      charGroupCircleRadius / 2 - 5
+
+    g.setColor(darkBackgroundCircle)
+    g.fillOval(80, 80, 2*backgroundCircleRadius, 2*backgroundCircleRadius)
+
+    val middleCircleRadius = charGroupCircleOffset
+    g.setColor(circleNormal)
+    g.fillOval(80 + backgroundCircleRadius - middleCircleRadius,
+      80 + backgroundCircleRadius - middleCircleRadius, 2*middleCircleRadius,
+      2* middleCircleRadius)
+
+    for((charGroup, i) <- chars.grouped(4).zipWithIndex) {
+      val xOffset = charGroupCircleOffset * Math.cos(2*Math.PI * ((i+6) % 8) / 8)
+      val yOffset = charGroupCircleOffset * Math.sin(2*Math.PI * ((i+6) % 8) / 8)
+      val posX = backgroundCircleRadius - charGroupCircleRadius + xOffset.toInt + 80
+      val posY = backgroundCircleRadius - charGroupCircleRadius + yOffset.toInt + 80
+
+      if(i == hightlightedCircle) {
+        drawCharsCircle(charGroup, colors, offsets, posX, posY,
+          charGroupCircleRadius, circleHighlighted, i, g)
+      } else {
+        drawCharsCircle(charGroup, transparent, offsets, posX, posY,
+          charGroupCircleRadius, circleNormal, i, g)
+      }
+    }
+  }
+
+  def drawCharsCircle(chars: Seq[String], colors: Seq[Color], offsets: Seq[(Int, Int)],
+    x: Int, y: Int, r: Int, circleColor: Color, index: Int, g: Graphics) {
+    g.setColor(circleColor)
+    g.fillOval(x, y, 2*r, 2*r)
+
+    val rect = (new Rectangle(0, 0, r, r)).transform(
+      Transform.createRotateTransform((Math.PI * (index-3)/4.).toFloat)).transform(
+      Transform.createTranslateTransform(x+r, y+r))
+
+    g.fill(rect)
+
+    val childRadius = 20
+    chars.zip(colors).zip(offsets).foreach {
+      case ((letter, color), offset) =>
+        drawCharCircle(letter, color, x + r + offset._1 - childRadius,
+          y + r + offset._2 - childRadius, childRadius, g)
+    }
+  }
+
+  def drawCharCircle(c: String, circleColor: Color, x: Int, y: Int, r: Int, g: Graphics) {
+    g.setColor(circleColor)
+    g.fillOval(x, y, r*2, r*2)
+
+    font.drawString(x + r - font.getWidth(c) /2 - 1,
+      y + r - font.getHeight(c)/2 - 1, c, Color.white)
   }
 
   var axes = Map[Int, Axis]()
@@ -134,15 +217,15 @@ class GameActor extends Actor {
   val mainThread = Config.system.actorOf(Props[MainThread], name = "mainActor")
 
   def receive = {
-    case "start" =>
+    case Start =>
+      // very important - use lwjgl for gamepad input!
       Input.disableControllers()
       gameLogic = new InputProviderTest()
+      gameLogic.inputHandler ! SubscribeUnhandled(self)
       mainThread ! gameLogic
 
-    case ButtonDown(controller, btn) =>
-      println("here")
+    case EventLink(ev @ ButtonDown(controller, btn), next) =>
       if(!gameLogic.controllersToDraw.contains(controller)) {
-        println("adding " + controller)
         gameLogic.controllersToDraw = (controller :: gameLogic.controllersToDraw).sorted
       }
 
@@ -150,7 +233,7 @@ class GameActor extends Actor {
         case PadButton.A => gameLogic.inputHandler ! Rumble(controller, (gameLogic.axes(controller).rightTrigger + 1) / 2)
         case PadButton.B => gameLogic.inputHandler ! Rumble(controller, 0.0f)
         case PadButton.Back => sys.exit(0)
-        case _ =>
+        case _ => next.head ! EventLink(ev, next.tail)
       }
   }
 }
@@ -167,5 +250,5 @@ object MyGame extends App {
 
   val game = Config.system.actorOf(Props[GameActor], name = "game")
   Config.system.eventStream.subscribe(game, classOf[ButtonEvent])
-  game ! "start"
+  game ! Start
 }
--- src/main/scala/GamepadActor.scala
+++ src/main/scala/GamepadActor.scala
@@ -5,6 +5,8 @@ import akka.actor._
 
 case object ReadAxis
 case object Poll
+case object Start
+case object Done
 
 // X = Left/Right, Y = Up/Down
 case class Axis(leftStickX: Float, leftStickY: Float,
@@ -25,17 +27,41 @@ case class ButtonUp(controller: Int, btn: PadButton) extends ButtonEvent
 
 case class Rumble(controller: Int, strength: Float)
 
+case class SubscribeAll(actor: ActorRef)
+case class SubscribeUnhandled(actor: ActorRef)
+case class Unsubscribe(actor: ActorRef)
+case class EventLink(e: ButtonEvent, next: Seq[ActorRef])
+
+class UnhandledEventActor extends Actor with ActorLogging {
+  def receive = {
+    case EventLink(e, Nil) => log.info(s"unhandled: $e")
+    case EventLink(e, next) => log.warning("I should be the last actor in the chain")
+  }
+}
+
 class InputActor extends Actor with ActorLogging {
+  val unhandledActor = Config.system.actorOf(Props[UnhandledEventActor], name = "unhandledActor")
+
   var controllers: Seq[Int] = (0 to 8)
+  var subscribers = List[ActorRef]()
+  var unhandled_subscribers = List[ActorRef](unhandledActor)
 
   def receive = {
-
-    case "start" =>
+    case Start =>
       Controllers.create()
       Controllers.clearEvents()
       controllers = (0 until Controllers.getControllerCount()).filter(c => Controllers.getController(c).getAxisCount() > 10)
-      sender ! "done"
+      sender ! Done
 
+    case SubscribeAll(sub) =>
+      subscribers = sub :: subscribers
+
+    case SubscribeUnhandled(sub) =>
+      unhandled_subscribers = sub :: unhandled_subscribers
+
+    case Unsubscribe(sub) =>
+      subscribers = subscribers.filter(_ != sub)
+      unhandled_subscribers = unhandled_subscribers.filter(_ != sub)
 
     case Poll =>
       Controllers.poll()
@@ -46,32 +72,30 @@ class InputActor extends Actor with ActorLogging {
           val componentI = Controllers.getEventControlIndex()
           if(Controllers.isEventButton()) {
             if(controller.isButtonPressed(componentI)) {
-              Config.system.eventStream.publish(ButtonDown(controllerI, translateButton(componentI)))
+              publish(ButtonDown(controllerI, translateButton(componentI)))
             } else {
-              Config.system.eventStream.publish(ButtonUp(controllerI, translateButton(componentI)))
+              publish(ButtonUp(controllerI, translateButton(componentI)))
             }
           } else {
             if(componentI <= 4) {
               if(controller.getAxisValue(componentI) == 1.0) {
-                Config.system.eventStream.publish(ButtonDown(controllerI, translateAxis(componentI)))
+                publish(ButtonDown(controllerI, translateAxis(componentI)))
               } else {
-                Config.system.eventStream.publish(ButtonUp(controllerI, translateAxis(componentI)))
+                publish(ButtonUp(controllerI, translateAxis(componentI)))
               }
             } else {
-              val newValue = Controllers.getEventSource().getAxisValue(componentI)
-              log.info(s"event for $componentI of $controllerI: $newValue")
+              //val newValue = Controllers.getEventSource().getAxisValue(componentI)
+              // new value for axis - don't create an event
             }
           }
         }
       }
-
 
     case Rumble(ctrl, strength) =>
       val controller = Controllers.getController(ctrl % 4)
       for(i <- 0 until controller.getRumblerCount()) {
         controller.setRumblerStrength(i, strength)
       }
-
 
     case ReadAxis =>
       val controllerAxes = controllers.map {
@@ -83,6 +107,14 @@ class InputActor extends Actor with ActorLogging {
           c -> Axis(axes(5), axes(6), axes(8), axes(9), axes(7), axes(10))
       }
       sender ! controllerAxes.toMap
+  }
+
+  def publish(ev: ButtonEvent) {
+    for(sub <- subscribers) {
+      sub ! ev
+    }
+    unhandled_subscribers.head ! EventLink(ev, unhandled_subscribers.tail)
+    // Config.system.eventStream.publish(ev)
   }
 
   def translateAxis(axis: Int) = axis match {
